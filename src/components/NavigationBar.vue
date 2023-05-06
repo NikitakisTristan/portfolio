8e9f411f6413178b189be0d9ee4c1c53200d070e
Tristan Nikitakis <tristan.nikitakis@gmail.com>
2023-05-05 23:05:40 -0400

    removing templated files created on project setup and styling, adding in pages that will be included in the app

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Tristan Nikitakis</h1>
+  </div>
+  <RouterLink v-for="route in router.getRoutes()" :to="route.path"> {{ route.name }}</RouterLink>
+</template>
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+}
+</style>
